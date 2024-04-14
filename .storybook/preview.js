9f44b1170e7e6388b520560ba836a660a230bc5e
Hiroyuki Maniwa <h.maniwa2010+biz@gmail.com>
2024-04-14 16:28:42 +0900

    Add: Bootstrap install
    Install The Base Bootstrap5

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,35 @@
 /** @type { import('@storybook/html').Preview } */
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Only import this if you want to use Bootstrap's
+// JQuery helpers
+import 'bootstrap/dist/js/bootstrap.bundle';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    expanded: true,
+    hideNoControlsWarning: true,
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-bs-theme',
+  }),
+];
+
 const preview = {
   parameters: {
     controls: {
