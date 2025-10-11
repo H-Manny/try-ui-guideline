015ccb6ec6db4eed9fee93b123ccd9aadf609c26
Hiroyuki Maniwa <h.maniwa2010+biz@gmail.com>
2025-10-11 16:48:45 +0900

    Docs: Add MDX and doc blocks documentation; update sidebar
    Added new documentation pages for MDX and Storybook doc blocks in the docs section. Updated the Astro config to autogenerate the sidebar from the docs directory, improving navigation and organization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,31 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      sidebar: [
+        {
+          label: 'Docs',
+          autogenerate: { directory: 'docs' },
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: 'MDX', slug: 'docs/index.mdx' },
+          // ],
+        },
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
